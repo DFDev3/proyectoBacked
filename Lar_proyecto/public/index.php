00a84233a410ccc6247e224b3b97c4d6245f8bb0
DFDev3 <ddiaz216@unab.edu.co>
2023-10-03 19:41:01 -0500

    ahora si

--- Lar_proyecto/public/index.php
+++ Lar_proyecto/public/index.php
@@ -54,28 +54,4 @@ $response = $kernel->handle(
 
 $kernel->terminate($request, $response);
 
-<?php
-
-require 'vendor/autoload.php';
-
-d([
-    'nombre' =>  'Faboan',
-    'apellido' => 'Suarez',
-    'institucion' => 'UNAB'
-]);
-
-Route::get('/', function () {
-    return "HOME PAGE";
-});
-Route::get('productos', function () {
-    return "LISTADO DE TODOS LOS PRODUCTOS";
-});
-Route::get('/producots/{id}', function ($id)) {
-    return "el nombre es : $id";
-});
-Route::get('/producots/create', function () {
-    return "aqui va el formulario : $id";
-});
-
-
-?>x
+?>
--- Lar_proyecto/routes/web.php
+++ Lar_proyecto/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/', function () {
+    return "HOME PAGE";
+});
+Route::get('productos', [ProductController::class, 'listProducts' ]);
+Route::get('details', [ProductController::class, 'details' ]);
+Route::get('/producots/{id}', function ($id){
+    return "el nombre es : $id";
+});
+Route::get('/producots/create', function () {
+    return "aqui va el formulario";
+});
+
+
+
+
+?>
